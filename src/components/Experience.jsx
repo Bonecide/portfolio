6ebdd46b5d0c3ebf2769b7f11329e67b5cdca224
Bonecide <marselkasymov23@gmail.com>
2023-11-28 15:06:09 +0600

    fixed mobile rotate room

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,8 +7,10 @@ import {
 import { useControls } from "leva";
 import { Avatar } from "./Avatar";
 import { Scene } from "./Scene";
-
+import { isDesktop, isTablet } from "react-device-detect";
+import { useState } from "react";
 export const Experience = () => {
+  const [width] = useState(window.innerWidth);
   const { animation } = useControls({
     animation: {
       value: "Typing",
@@ -18,14 +20,14 @@ export const Experience = () => {
   return (
     <>
       <OrbitControls
-        enablePan={false}
+        enableRotate={width > 800}
         autoRotate={true}
         autoRotateSpeed={1} // Adjust this value
         enableZoom={false}
       />
       <Sky />
       <Environment preset="sunset" />
-      <group position-y={-1}>
+      <group scale={width > 800 ? 1 : 0.6} position-y={-1}>
         <group position={[-0.2, 0, -0.7]} rotation-y={Math.PI * 0.9}>
           <Avatar animation={animation} />
         </group>
